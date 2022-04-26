cdcc17d6a70cca8a0c6afe1f3ea249f258e11d59
Roberta de Siqueira <designer.roberta@gmail.com>
2022-04-25 21:30:04 -0300

    update files project, ORM mapping, database seeding

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Product.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Product.java
@@ -53,6 +53,16 @@ public class Product implements Serializable {
 		this.date = date;
 		this.categories = categories;
 	}
+	
+	public Product(Long id, String name, String description, Double price, String imgUrl, Instant date) {
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.imgUrl = imgUrl;
+		this.date = date;
+		this.categories = categories;
+	}
 
 	public Long getId() {
 		return id;
